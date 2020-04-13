ab362d10ba0b322ac7b7b4e45aec287c1dac8b91
ripraprup10 <ripraprup10@gmail.com>
2020-04-13 14:17:09 +0200

    font, style and colors. Start of statsactivity

---
+++ app/src/main/java/com/example/food_waste_prototype/StatsActivity.java
@@ -0,0 +1,15 @@
+package com.example.food_waste_prototype;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StatsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
