71d6f0689a0dcbbe9332d6e39f0ed0e05bce01ea
Alexcool3 <36537085+Alexcool3@users.noreply.github.com>
2020-04-15 19:13:26 +0200

    Input Activity update - Dialog updated and activity added to AndroidManifest.xml

--- app/src/main/java/com/example/food_waste_prototype/InputActivity.java
+++ app/src/main/java/com/example/food_waste_prototype/InputActivity.java
@@ -1,25 +1,40 @@
 package com.example.food_waste_prototype;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class InputActivity extends AppCompatActivity{
-
-    final Context context = this;
 
     // Initial Food category class
     public class Food{
         private float weight;
         private float price;
+        private float pricePerUnit;
         private float fwn; // Food Waste Number
         String name;
 
@@ -52,32 +67,82 @@ public class InputActivity extends AppCompatActivity{
             return fwn;
         }
 
+        public void SetPricePerUnit(float pricePerUnit){
+            this.pricePerUnit = pricePerUnit;
+        }
+
+        public float GetPricePerUnit(){
+            return pricePerUnit;
+        }
+
+        public void SetName(String name){
+            this.name = name;
+        }
+
+        public String GetName(){
+            return name;
+        }
+
         ImageButton foodButton;
     }
 
     // List containing all food category instances
-    List<Food> foodCategories;
+    ArrayList<Food> foodCategories = new ArrayList<Food>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
+        final Context context = InputActivity.this;
+
+        // Screen size
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
 
         ImageButton addButton = findViewById(R.id.addButton);
 
+        // Add new category button + dialog
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
 
                 // Set the title of the dialog
+                final Context context = alertDialogBuilder.getContext();
+                LinearLayout linearLayout = new LinearLayout(context);
+                linearLayout.setOrientation(LinearLayout.VERTICAL);
+
                 alertDialogBuilder.setTitle("Ny Kategori");
                 alertDialogBuilder.setMessage("Tryk på bekræft for at oprette kategorien");
                 alertDialogBuilder.setCancelable(false);
+
+                final EditText foodCategoryName = new EditText(InputActivity.this);
+                foodCategoryName.setHint("Navn");
+                foodCategoryName.setInputType(InputType.TYPE_CLASS_TEXT);
+                alertDialogBuilder.setView(foodCategoryName);
+                linearLayout.addView(foodCategoryName);
+
+                final EditText price = new EditText(InputActivity.this);
+                price.setHint("Pris per kilo");
+                price.setInputType(InputType.TYPE_CLASS_NUMBER);
+                alertDialogBuilder.setView(price);
+                linearLayout.addView(price);
+
+                alertDialogBuilder.setView(linearLayout);
+
                 alertDialogBuilder.setPositiveButton("Bekræft", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        InputActivity.this.finish();
+                        Food food = new Food(foodCategoryName.getText().toString());
+                        food.SetPricePerUnit(price.getText().toString().matches("") ? 0.0f : Float.valueOf(price.getText().toString()));
+                        //Log.d("New Category Test", "name: " + food.GetName() + "price per unit: " + food.GetPricePerUnit());
+                        food.foodButton = new ImageButton(InputActivity.this);
+                        //food.foodButton.setImageDrawable();
+                        //food.foodButton.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+                        foodCategories.add(food);
                     }
                 });
                 alertDialogBuilder.setNegativeButton("Fortryd", new DialogInterface.OnClickListener() {
@@ -86,6 +151,12 @@ public class InputActivity extends AppCompatActivity{
                         dialog.cancel();
                     }
                 });
+
+
+
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                alertDialog.show();
             }
         });
     }
--- app/src/main/java/com/example/food_waste_prototype/StatsActivity.java
+++ app/src/main/java/com/example/food_waste_prototype/StatsActivity.java
@@ -6,10 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class StatsActivity extends AppCompatActivity {
 
-
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
     }
+     */
 }
