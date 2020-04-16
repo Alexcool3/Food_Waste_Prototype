ad02083dc303b0821b67e072b98831ba125702e5
Seje Nikolaj xP <npeter17@student.aau.dk>
2020-04-16 23:27:18 +0200

    LoginActivity

--- app/src/main/java/com/example/food_waste_prototype/LoginActivity.java
+++ app/src/main/java/com/example/food_waste_prototype/LoginActivity.java
@@ -1,16 +1,51 @@
 package com.example.food_waste_prototype;
 
 
+import android.app.AppComponentFactory;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.app.Activity;
-
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LoginActivity extends Activity{
-    Button b1, b2;
-    EditText ed1, ed2;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class LoginActivity extends AppCompatActivity {
+    private EditText brugernavn;
+    private EditText password;
+    private Button login;
+    private Button registrer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        brugernavn = (EditText)findViewById(R.id.etBrugernavn);
+        password = (EditText)findViewById(R.id.etPassword);
+        login = (Button)findViewById(R.id.buttonLogin);
+        registrer = (Button)findViewById(R.id.buttonRegistrer);
+        
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                validate(brugernavn.getText().toString(), password.getText().toString());
+            }
+        });
+
+
+    }
+
+    private void validate(String inputBrugernavn, String inputPassword) {
+        if((inputBrugernavn == "Nikolaj") && (inputPassword == "ersej")){
+            Intent intent = new Intent(LoginActivity.this, InputActivity.class);
+            startActivity(intent);
+        } else {
+            login.setEnabled(false);
+        }
+    }
 
 }
