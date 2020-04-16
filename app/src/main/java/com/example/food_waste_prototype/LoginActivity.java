c9041a9c8a6b183c59c90bfa93e1976179b3744f
Seje Nikolaj xP <npeter17@student.aau.dk>
2020-04-16 22:41:54 +0200

    LoginActivity

--- app/src/main/java/com/example/food_waste_prototype/LoginActivity.java
+++ app/src/main/java/com/example/food_waste_prototype/LoginActivity.java
@@ -1,5 +1,16 @@
 package com.example.food_waste_prototype;
 
-public class LoginActivity {
-    String hej;
+
+import android.view.View;
+import android.widget.EditText;
+import android.app.Activity;
+
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity{
+    Button b1, b2;
+    EditText ed1, ed2;
+
+
 }
