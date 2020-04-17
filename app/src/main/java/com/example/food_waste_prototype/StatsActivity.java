a6b80dfb0c89ad4126f231946eb16153c324f7fb
Adamsen5482 <ripraprup10@gmail.com>
2020-04-17 20:59:27 +0200

    Refactor and simplification, comments

--- app/src/main/java/com/example/food_waste_prototype/StatsActivity.java
+++ app/src/main/java/com/example/food_waste_prototype/StatsActivity.java
@@ -1,28 +1,19 @@
 package com.example.food_waste_prototype;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.media.Image;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.AdapterView.OnItemSelectedListener;
-
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.PieData;
@@ -30,14 +21,6 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONArray;
-import org.w3c.dom.Text;
-
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 
 public class StatsActivity extends AppCompatActivity {
@@ -49,6 +32,7 @@ public class StatsActivity extends AppCompatActivity {
     String[] days = new String[]{"Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"};
     String[] months = new String[]{"Januar", "Februar", "Marts", "April", "Maj", "Juni", "Juli","August", "September", "Oktober", "November", "December" };
     String[] years = new String[]{"2016", "2017", "2018", "2019", "2020"};
+
     public StatsActivity() {
 
     }
@@ -59,16 +43,24 @@ public class StatsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stats);
         final Context context = StatsActivity.this;
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); // make the app fullscreen
-        TaskBarButtons(); // activate the taskbar buttons
+        SetupButtons();
         FillTable();
-        ToggleButton();
-        DropDown();
-        SettingsButton();
-
     }
 
-    private void DropDown() {
+    private void SetupButtons(){
+
+        ImageButton leftArrow =  findViewById(R.id.button_arrow_left);
+        ImageButton rightArrow =  findViewById(R.id.button_arrow_right);
+        ImageButton inputtaskbarbutton = findViewById(R.id.button_input);
+        final ImageButton switchbutton = findViewById(R.id.button_switch);
+        final TableLayout table = findViewById(R.id.tableLayout);
+        final ImageButton settingsButton = findViewById(R.id.button_options);
+        final ImageButton toggle = findViewById(R.id.button_toggle);
+        final TextView toggletext = findViewById(R.id.toggleText);
         final Spinner dropdown = findViewById(R.id.dropdown);
+        final PieChart pieChart = findViewById(R.id.pieChart);
+
+        //setup the dropdown
         String[] items = new String[]{"Dag", "Måned", "År"};
         ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(StatsActivity.this, R.layout.spinner_text, items );
         adapter.setDropDownViewResource(R.layout.simple_spinner_dropdown);
@@ -79,29 +71,20 @@ public class StatsActivity extends AppCompatActivity {
                             AdapterView<?> parent, View view, int position, long id) {
                         setDate("left", dropdown);
                     }
-
                     public void onNothingSelected(AdapterView<?> parent) {
-                       // showToast("Spinner1: unselected");
+                        // showToast("Spinner1: unselected");
                     }
                 });
-            Arrows(dropdown);
-    }
 
-    private void Arrows(final Spinner dropdown) {
-
-
-        ImageButton leftArrow =  findViewById(R.id.button_arrow_left);
-        ImageButton rightArrow =  findViewById(R.id.button_arrow_right);
-
-
-
-
+        // arrow to the left of the label
         leftArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 setDate("left", dropdown);
             }
         });
+
+        // arrow to the right of the label
         rightArrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -109,14 +92,53 @@ public class StatsActivity extends AppCompatActivity {
             }
         });
 
+        // button that leads to inputpage
+        inputtaskbarbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchActivity("input");
+            }
+        });
+
+        // food waste - food scraps toogle
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchToggle(toggle, toggletext);
+            }
+        });
+
+        // switching between table
+        switchbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchTable(switchbutton, table, pieChart);
+            }
+        });
+
+        // options button
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SwitchActivity("settings");
+            }
+        });
     }
 
     private void setDate(String direction, Spinner dropdown){
 
-
-
+        // get the current selected item from dropdown
         String text = dropdown.getSelectedItem().toString();
+
+        // array to contain days, months or years based on dropdown
         final String[] targetarray;
+
+        // current position in array
+        int pos = 0;
+
+        // label between the arrows
+        TextView date = findViewById(R.id.date);
+
         if(text.equals("Dag")){
             targetarray=days;
         } else if(text.equals("Måned")){
@@ -125,29 +147,23 @@ public class StatsActivity extends AppCompatActivity {
             targetarray=years;
         }
 
-
-        int pos = 0;
-        TextView date = findViewById(R.id.date);
+        // find out whats in the label and set the current pos to that
         for(int i = 0; i < targetarray.length; i++) {
             if (date.getText().equals(targetarray[i])){
                 pos=i;
-
             }
         }
 
-
-
-
-
+        // move the entry to the left one
         if(direction.equals("left")){
             if(pos>-1){
-
                 if(pos==0){
                     pos=targetarray.length;
                 }
                 date.setText(targetarray[pos-1]);
             }
         }
+        // move the entry to the right one
         else {
             if (pos < targetarray.length) {
 
@@ -159,46 +175,9 @@ public class StatsActivity extends AppCompatActivity {
         }
     }
 
-
-    private void TaskBarButtons() {
-
-
-        ImageButton inputtaskbarbutton = findViewById(R.id.button_input);
+    private void SwitchTable(ImageButton switchy, TableLayout table, PieChart pieChart) {
 
 
-        inputtaskbarbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SwitchActivity("input");
-            }
-        });
-    }
-
-    private void ToggleButton() {
-
-        final ImageButton switchbutton = findViewById(R.id.button_switch);
-        final TableLayout table = findViewById(R.id.tableLayout);
-       // final ImageView piechart = findViewById(R.id.piechart);
-
-        final ImageButton toggle = findViewById(R.id.button_toggle);
-        final TextView toggletext = findViewById(R.id.toggleText);
-        toggle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SwitchToggle(toggle, toggletext);
-            }
-        });
-        switchbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SwitchTable(switchbutton, table);
-            }
-        });
-    }
-
-    private void SwitchTable(ImageButton switchy, TableLayout table) {
-
-        PieChart pieChart = findViewById(R.id.pieChart);
         drawChart((pieChart));
 
         if (table.getVisibility() == View.VISIBLE) {
@@ -223,57 +202,9 @@ public class StatsActivity extends AppCompatActivity {
         }
     }
 
-
-    private void SettingsButton() {
-
-        final ImageButton settingsButton = findViewById(R.id.button_options);
-        settingsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SwitchActivity("settings");
-            }
-        });
-    }
-
     private void FillTable() {
         TextView employeeName, employeeSalary;
-        try {
-            JSONArray mJsonArray = new JSONArray(loadJSONFromAsset());
-            for (int i = 0; i < mJsonArray.length(); i++) {
-                JSONObject mJsonObject = mJsonArray.getJSONObject(i);
-                JSONArray mJsonArrayProperty = mJsonObject.getJSONArray("properties");
-                for (int j = 0; j < mJsonArrayProperty.length(); j++) {
-                    JSONObject mJsonObjectProperty = mJsonArrayProperty.getJSONObject(i);
 
-                    names.add(mJsonObjectProperty.getString("name"));
-
-
-                }
-            }
-
-        } catch (JSONException ex) {
-            ex.printStackTrace();
-            Toast.makeText(StatsActivity.this,
-                    "Failed", Toast.LENGTH_LONG).show();
-        }
-        //Toast.makeText(StatsActivity.this,
-        // names.get(0), Toast.LENGTH_LONG).show();
-    }
-
-    public String loadJSONFromAsset() {
-        String json = null;
-        try {
-            InputStream is = getResources().openRawResource(R.raw.foodwaste);
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        return json;
     }
 
     private void SwitchActivity(String target) {
@@ -286,7 +217,6 @@ public class StatsActivity extends AppCompatActivity {
         }
         if (target == "input") {
 
-
             Intent intent = new Intent(this, InputActivity.class);
 
             startActivity(intent);
@@ -294,7 +224,6 @@ public class StatsActivity extends AppCompatActivity {
     }
 
     private void drawChart(PieChart pieChart) {
-
 
         pieChart.setUsePercentValues(true);
 
@@ -320,6 +249,5 @@ public class StatsActivity extends AppCompatActivity {
         dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
         data.setValueTextSize(13f);
         data.setValueTextColor(Color.DKGRAY);
-
     }
 }
