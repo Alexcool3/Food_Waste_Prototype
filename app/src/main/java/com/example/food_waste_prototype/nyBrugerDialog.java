c468aa35296ebae588f2c57a65c16c9927abce4f
Adamsen5482 <ripraprup10@gmail.com>
2020-04-22 10:16:27 +0200

    ny bruger fix

--- app/src/main/java/com/example/food_waste_prototype/nyBrugerDialog.java
+++ app/src/main/java/com/example/food_waste_prototype/nyBrugerDialog.java
@@ -32,10 +32,7 @@ public class nyBrugerDialog  extends  AlertDialog{
         AlertDialog.setView(newView);
         final AlertDialog dialog = AlertDialog.create();
         dialog.show();
-
-        //TextView explainer = newView.findViewById(R.id.dialog_info);
-       // explainer.setText((text));
-
+        
         editTextBrugernavn = newView.findViewById(R.id.edit_brugernavn);
         editTextKodeord = newView.findViewById(R.id.edit_kodeord);
 
@@ -51,66 +48,3 @@ public class nyBrugerDialog  extends  AlertDialog{
         dialog.cancel();
     }
 }
-
-/*public class nyBrugerDialog extends AppCompatDialogFragment {
-    private EditText editTextBrugernavn;
-    private EditText editTextKodeord;
-    private Button button;
-
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = new inflater.inflate(R.layout.nybrugerdialog, null);
-
-        builder.setView(view)
-                .setTitle("Login")
-                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                })
-                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-
-                    }
-                });
-        editTextBrugernavn = view.findViewById(R.id.edit_brugernavn);
-        editTextKodeord = view.findViewById(R.id.edit_kodeord);
-
-        return builder.create();
-
-    }
-}
-*/
-
-/*
-public class nyBrugerDialog extends AppCompatActivity {
-    private TextView tv_brugernavn;
-    private TextView tv_kodeord;
-    private Button button;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.nybrugerdialog);
-
-        tv_brugernavn = (TextView) findViewById(R.id.tv_brugernavn);
-        tv_kodeord = (TextView) findViewById(R.id.tv_kodeord);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openDialog();
-            }
-        });
-    }
-    public void openDialog() {
-        nyBrugerDialog exampleDialog = new nyBrugerDialog();
-        exampleDialog.show(getSupportFragmentManager(),"example dialog");
-    }
-}
-*/
